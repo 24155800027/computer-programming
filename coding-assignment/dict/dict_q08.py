febf2f0016ba039f960062eb05a4694ad035a641
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 08:28:07 +0530

    Update dict_q08.py

--- coding-assignment/dict/dict_q08.py
+++ coding-assignment/dict/dict_q08.py
@@ -1,1 +1,4 @@
-
+#Find the intersection of two lists using a dictionary.
+def intersection(lst1, lst2):
+    dict1 = {item: True for item in lst1}
+    return [item for item in lst2 if item in dict1]
