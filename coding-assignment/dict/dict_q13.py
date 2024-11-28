3d2bf15db6543c6b24e6d71d91f57f64082f08bd
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 08:31:03 +0530

    Update dict_q13.py

--- coding-assignment/dict/dict_q13.py
+++ coding-assignment/dict/dict_q13.py
@@ -1,1 +1,3 @@
-
+#You are given two lists. One represents the keys and the other represents the values. Create a dictionary using these lists.
+def create_dict(keys, values):
+    return dict(zip(keys, values))
