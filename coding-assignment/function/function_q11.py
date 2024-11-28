4464d76bad92a7b5746efe6c92a2d8cd8249f3f5
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 08:58:24 +0530

    Update function_q11.py

--- coding-assignment/function/function_q11.py
+++ coding-assignment/function/function_q11.py
@@ -1,1 +1,6 @@
+#Write a function to find the second largest number in a list.
 
+def second_largest(lst):
+    unique = list(set(lst))
+    unique.sort()
+    return unique[-2] if len(unique) > 1 else None
