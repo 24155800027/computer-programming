7fb18a551c138115bc03caced49090acaf534eea
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 08:27:37 +0530

    Update dict_q07.py

--- coding-assignment/dict/dict_q07.py
+++ coding-assignment/dict/dict_q07.py
@@ -1,1 +1,8 @@
-
+#Given a list of strings, group anagrams together using a dictionary.
+from collections import defaultdict
+def group_anagrams(strs):
+    anagrams = defaultdict(list)
+    for word in strs:
+        sorted_word = ''.join(sorted(word))
+        anagrams[sorted_word].append(word)
+    return list(anagrams.values())
