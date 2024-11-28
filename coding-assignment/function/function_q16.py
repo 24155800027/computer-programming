44d3cb77c62c6608efd8724f8fbb3230c425316e
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 09:01:02 +0530

    Update function_q16.py

--- coding-assignment/function/function_q16.py
+++ coding-assignment/function/function_q16.py
@@ -1,1 +1,8 @@
+#write a function to calculate the power of a number without using **.
 
+
+def power(base, exp):
+    result = 1
+    for _ in range(exp):
+        result *= base
+    return result
