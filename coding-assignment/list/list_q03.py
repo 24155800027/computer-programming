5d7e4b8da7c125b7fa5aacd04c8bd50dea46e1b5
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 17:45:39 +0530

    Update list_q03.py

--- coding-assignment/list/list_q03.py
+++ coding-assignment/list/list_q03.py
@@ -1,1 +1,6 @@
+#Find the second largest element in a list of integers.
 
+def second_largest(lst):
+    unique = list(set(lst))
+    unique.sort()
+    return unique[-2] if len(unique) >= 2 else None
