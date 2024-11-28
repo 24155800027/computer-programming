c4331441f60c4a11746a4e3b64c3e9bddce76964
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 08:34:35 +0530

    Update dict_q19.py

--- coding-assignment/dict/dict_q19.py
+++ coding-assignment/dict/dict_q19.py
@@ -1,1 +1,7 @@
+#Create a dictionary where the keys are words and the values are their frequencies from a list of words
+def frequency_dict(words):
+    freq = {}
+    for word in words:
+        freq[word] = freq.get(word, 0) + 1
+    return freq
 
