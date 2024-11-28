1b45b60e1918523bbf0a34521ba3773d8985f043
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 08:35:17 +0530

    Update dict_q20.py

--- coding-assignment/dict/dict_q20.py
+++ coding-assignment/dict/dict_q20.py
@@ -1,1 +1,4 @@
+#Write a program that returns a dictionary containing the keys that are in the first dictionary but not in the second one.
+def dict_difference(dict1, dict2):
+    return {key: dict1[key] for key in dict1 if key not in dict2}
 
