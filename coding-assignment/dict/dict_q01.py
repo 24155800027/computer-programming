409a648dfb7e57f53b02346acc4ec48d5abe104e
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 08:17:10 +0530

    Update dict_q01.py

--- coding-assignment/dict/dict_q01.py
+++ coding-assignment/dict/dict_q01.py
@@ -1,1 +1,11 @@
+#You are given a list of integers. Write a Python program to count the frequency of each number using a dictionary.
+#solution:
 
+def count_frequency(lst):
+    freq = {}
+    for num in lst:
+        if num in freq:
+            freq[num] += 1
+        else:
+            freq[num] = 1
+    return freq
