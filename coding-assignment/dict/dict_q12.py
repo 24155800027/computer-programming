760a74126ec9d756677bcdbcd04197f32674e9e9
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 08:30:29 +0530

    Update dict_q12.py

--- coding-assignment/dict/dict_q12.py
+++ coding-assignment/dict/dict_q12.py
@@ -1,1 +1,3 @@
-
+#Write a program to find the intersection of two dictionaries, i.e., common keys with the same values.
+def dict_intersection(dict1, dict2):
+    return {key: dict1[key] for key in dict1 if key in dict2 and dict1[key] == dict2[key]}
