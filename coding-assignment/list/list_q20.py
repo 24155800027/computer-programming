bf4eac3556e001696879d40887646e7c0be20177
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 17:53:18 +0530

    Update list_q20.py

--- coding-assignment/list/list_q20.py
+++ coding-assignment/list/list_q20.py
@@ -1,1 +1,11 @@
+#Rearrange a list such that it alternates between the largest and smallest elements.
 
+def rearrange_alternate(lst):
+    lst.sort()
+    result = []
+    while lst:
+        if lst:
+            result.append(lst.pop(-1))
+        if lst:
+            result.append(lst.pop(0))
+    return result
