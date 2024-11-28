53abc7b6bd0e3c868b9e611165335f30b990e096
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 08:26:20 +0530

    Update dict_q05.py

--- coding-assignment/dict/dict_q05.py
+++ coding-assignment/dict/dict_q05.py
@@ -1,1 +1,7 @@
-
+#Write a Python program to count the occurrences of each word in a string using a dictionary.
+def word_count(string):
+    words = string.split()
+    word_dict = {}
+    for word in words:
+        word_dict[word] = word_dict.get(word, 0) + 1
+    return word_dict
