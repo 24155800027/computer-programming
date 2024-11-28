cd3d43e7e224e4c15969ac62dd3b7de964072956
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 17:52:03 +0530

    Update list_q17.py

--- coding-assignment/list/list_q17.py
+++ coding-assignment/list/list_q17.py
@@ -1,1 +1,8 @@
+#Generate all possible subsets of a given list.
 
+from itertools import combinations
+def generate_subsets(lst):
+    subsets = []
+    for i in range(len(lst) + 1):
+        subsets.extend(combinations(lst, i))
+    return subsets
