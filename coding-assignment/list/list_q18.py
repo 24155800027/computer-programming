de448af4ed0c9113e61b1876fd70bbe013d23cff
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 17:52:29 +0530

    Update list_q18.py

--- coding-assignment/list/list_q18.py
+++ coding-assignment/list/list_q18.py
@@ -1,1 +1,11 @@
+#Find the longest increasing subsequence in a list.
 
+def longest_increasing_subsequence(lst):
+    if not lst:
+        return []
+    dp = [1] * len(lst)
+    for i in range(len(lst)):
+        for j in range(i):
+            if lst[i] > lst[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    return max(dp)
