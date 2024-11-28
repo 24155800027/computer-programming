89e1869563400fe759ddb0825e7034d8d6743201
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 08:28:39 +0530

    Update dict_q09.py

--- coding-assignment/dict/dict_q09.py
+++ coding-assignment/dict/dict_q09.py
@@ -1,1 +1,5 @@
-
+#Sort a list of elements based on their frequency using a dictionary.
+from collections import Counter
+def sort_by_frequency(lst):
+    freq = Counter(lst)
+    return sorted(lst, key=lambda x: freq[x], reverse=True)
