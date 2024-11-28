fd126cbec3803bf694c9afac90342f56511c1bb7
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 17:52:52 +0530

    Update list_q19.py

--- coding-assignment/list/list_q19.py
+++ coding-assignment/list/list_q19.py
@@ -1,1 +1,9 @@
+#Find all elements in a list that appear more than n/k times.
+
+
+from collections import Counter
+def find_elements(lst, k):
+    n = len(lst)
+    freq = Counter(lst)
+    return [key for key, count in freq.items() if count > n // k]
 
