a774afc50f05bdf4aa7e2e11de19814b01c2fd22
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 08:24:11 +0530

    Update dict_q02.py

--- coding-assignment/dict/dict_q02.py
+++ coding-assignment/dict/dict_q02.py
@@ -1,1 +1,9 @@
-
+#Write a Python program to find the element that occurs most frequently in a list using a dictionary.
+def most_frequent(lst):
+    freq = {}
+    for num in lst:
+        if num in freq:
+            freq[num] += 1
+        else:
+            freq[num] = 1
+    return max(freq, key=freq.get)
