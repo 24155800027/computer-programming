63b56bc15ba919741aa574a3813848be4ae71ed5
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 17:51:33 +0530

    Update list_q16.py

--- coding-assignment/list/list_q16.py
+++ coding-assignment/list/list_q16.py
@@ -1,1 +1,11 @@
+#Find all duplicate elements in a list.
 
+def find_duplicates(lst):
+    seen = set()
+    duplicates = set()
+    for num in lst:
+        if num in seen:
+            duplicates.add(num)
+        else:
+            seen.add(num)
+    return list(duplicates)
