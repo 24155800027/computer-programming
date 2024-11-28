d0eeab4323ed7185c375e90575fff44f3d2f5075
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 08:25:46 +0530

    Update dict_q04.py

--- coding-assignment/dict/dict_q04.py
+++ coding-assignment/dict/dict_q04.py
@@ -1,1 +1,9 @@
-
+#Given two lists, determine if the second list is a subset of the first one using dictionaries.
+def is_subset(list1, list2):
+    dict1 = {}
+    for item in list1:
+        dict1[item] = 1
+    for item in list2:
+        if item not in dict1:
+            return False
+    return True
