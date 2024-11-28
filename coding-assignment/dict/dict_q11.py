5ccb2471017b2431e493ecce3828ef3ff638d967
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 08:29:53 +0530

    Update dict_q11.py

--- coding-assignment/dict/dict_q11.py
+++ coding-assignment/dict/dict_q11.py
@@ -1,1 +1,7 @@
-
+#Write a program to count the number of unique words in a sentence using a dictionary.
+def count_unique_words(sentence):
+    words = sentence.split()
+    word_dict = {}
+    for word in words:
+        word_dict[word] = 1
+    return len(word_dict)
