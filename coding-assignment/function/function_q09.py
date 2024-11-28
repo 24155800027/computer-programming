e17cc417db12a74d39a032066c64176261a635de
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 08:57:28 +0530

    Update function_q09.py

--- coding-assignment/function/function_q09.py
+++ coding-assignment/function/function_q09.py
@@ -1,1 +1,6 @@
+#Write a function to check if a number is an Armstrong number.
 
+
+def is_armstrong(num):
+    digits = list(map(int, str(num)))
+    return num == sum(d ** len(digits) for d in digits)
