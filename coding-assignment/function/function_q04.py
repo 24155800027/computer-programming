631a1758725f7306cd27148140a1cb3e0eab0d55
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 08:55:03 +0530

    Update function_q04.py

--- coding-assignment/function/function_q04.py
+++ coding-assignment/function/function_q04.py
@@ -1,1 +1,8 @@
+# Write a function to generate the first n Fibonacci numbers.
 
+
+def fibonacci(n):
+    fib = [0, 1]
+    for i in range(2, n):
+        fib.append(fib[-1] + fib[-2])
+    return fib[:n]
