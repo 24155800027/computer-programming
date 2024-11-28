f766745032d9a7f5d396af6828080a424e2c2e56
24155800027 <chetansaraswat1234@gmail.com>
2024-11-28 17:50:17 +0530

    Update list_q13.py

--- coding-assignment/list/list_q13.py
+++ coding-assignment/list/list_q13.py
@@ -1,1 +1,11 @@
+#Find all pairs of numbers in a list that add up to a target sum.
 
+def find_pairs(lst, target_sum):
+    pairs = []
+    seen = set()
+    for num in lst:
+        diff = target_sum - num
+        if diff in seen:
+            pairs.append((diff, num))
+        seen.add(num)
+    return pairs
